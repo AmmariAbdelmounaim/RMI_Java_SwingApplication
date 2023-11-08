abdb9fe1463c586a5563b9438e2eb1e44710006e
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-11-08 11:56:45 +0100

    Initial commit

---
+++ BankManager.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface BankManager extends Remote {
+    void creerCompte(String id,double somme) throws RemoteException;
+    void ajouter(String id,double somme) throws RemoteException;
+    void retirer(String id,double somme) throws  RemoteException;
+    double consulterSolde(String id) throws  RemoteException;
+    double transfererSolde(String id_C,String id_D,double somme) throws  RemoteException;
+}
